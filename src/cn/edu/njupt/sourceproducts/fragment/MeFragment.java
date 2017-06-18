06c7fa81c2db1112a3a9c1b284cb2bd9d89e7530
873459621 <873459621@qq.com>
2017-06-18 13:40:38 +0800

    20170618

--- src/cn/edu/njupt/sourceproducts/fragment/MeFragment.java
+++ src/cn/edu/njupt/sourceproducts/fragment/MeFragment.java
@@ -23,14 +23,19 @@ public class MeFragment extends Fragment {
 
 	private View mView;
 	private TextView tv_personal;
+	private TextView tv_all_orders;
+	private TextView tv_non_payment;
+	private TextView tv_unfilled;
+	private TextView tv_unrecrived;
+	private TextView tv_unvalued;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		mView = inflater.inflate(R.layout.fragment_me, container, false);
-		
+
 		initUI();
-		
+
 		return mView;
 	}
 
@@ -50,6 +55,12 @@ public class MeFragment extends Fragment {
 	 */
 	private void initUI() {
 		tv_personal = (TextView) mView.findViewById(R.id.tv_personal);
+		tv_all_orders = (TextView) mView.findViewById(R.id.tv_all_orders);
+		tv_non_payment = (TextView) mView.findViewById(R.id.tv_non_payment);
+		tv_unfilled = (TextView) mView.findViewById(R.id.tv_unfilled);
+		tv_unrecrived = (TextView) mView.findViewById(R.id.tv_unrecrived);
+		tv_unvalued = (TextView) mView.findViewById(R.id.tv_unvalued);
+
 		boolean has_logged_in = SPUtils.getBoolean(getActivity(),
 				ConstantValue.HAS_LOGGED_IN, false);
 
@@ -57,7 +68,7 @@ public class MeFragment extends Fragment {
 			String username = SPUtils.getString(getActivity(),
 					ConstantValue.USERNAME, null);
 			tv_personal.setText("您好：" + username);
-			
+
 		} else {
 			tv_personal.setOnClickListener(new OnClickListener() {
 
@@ -69,6 +80,60 @@ public class MeFragment extends Fragment {
 				}
 			});
 		}
+
+		tv_all_orders.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				reset();
+			}
+		});
+
+		tv_non_payment.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				reset();
+				tv_non_payment.setSelected(true);
+			}
+		});
+
+		tv_unfilled.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				reset();
+				tv_unfilled.setSelected(true);
+			}
+		});
+
+		tv_unrecrived.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				reset();
+				tv_unrecrived.setSelected(true);
+			}
+		});
+
+		tv_unvalued.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				reset();
+				tv_unvalued.setSelected(true);
+			}
+		});
+	}
+
+	/**
+	 * 重置选中状态
+	 */
+	private void reset() {
+		tv_non_payment.setSelected(false);
+		tv_unfilled.setSelected(false);
+		tv_unrecrived.setSelected(false);
+		tv_unvalued.setSelected(false);
 	}
 
 }
